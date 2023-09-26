ca905dab31ca160264134f8ff2f346556ec313cc
Learnedprawn <88869609+Learnedprawn@users.noreply.github.com>
2023-09-26 23:59:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("HELLO")
+def fancy_function():
+    """This function does nothing but looks great."""
+    for _ in range(5):
+        print("Beautiful code")
+    
+class PrettyClass:
+    def __init__(self):
+        self.attribute = "Aesthetic"
+    
+    def do_nothing(self):
+        pass
+
+if __name__ == "__main__":
+    fancy_function()
+    obj = PrettyClass()
+    print(obj.attribute)
